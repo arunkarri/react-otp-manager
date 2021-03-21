1c6bb69e02cecbee3ca9fd59f6b3effa0e4b12e6
Arun Kashyap Karri <arun.kashyap@dltlabs.io>
2021-03-21 18:10:26 +0530

    Updates

---
+++ precache-manifest.77dad771ea071339f0bc00a746cd61c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c02c673ee949dbf45bacc3b21b2e76",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ae5398e94711edec868c",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "c3e7becc4702e3b9a04c",
+    "url": "./static/js/2.b187e088.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b187e088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae5398e94711edec868c",
+    "url": "./static/js/main.8292c37a.chunk.js"
+  },
+  {
+    "revision": "8ca23cd8144aad406773",
+    "url": "./static/js/runtime-main.55345e1a.js"
+  }
+]);
